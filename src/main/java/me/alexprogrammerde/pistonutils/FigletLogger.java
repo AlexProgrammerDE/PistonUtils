514eaac604d7f1278af1f0648bf31e440a28076f
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-01-19 14:38:26 +0100

    Add figlet logger

---
+++ src/main/java/me/alexprogrammerde/pistonutils/FigletLogger.java
@@ -0,0 +1,24 @@
+package me.alexprogrammerde.pistonutils;
+
+import com.github.dtmo.jfiglet.FigFontResources;
+import com.github.dtmo.jfiglet.FigletRenderer;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+
+@SuppressWarnings({"unused"})
+public class FigletLogger {
+    private FigletLogger() {}
+
+    public static void logInBig(JavaPlugin plugin) {
+        try {
+            final FigletRenderer figletRenderer = new FigletRenderer(FigFontResources.loadFigFontResource(FigFontResources.STANDARD_FLF));
+
+            figletRenderer.setSmushMode(100);
+
+            plugin.getLogger().info(figletRenderer.renderText(plugin.getDescription().getName()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
