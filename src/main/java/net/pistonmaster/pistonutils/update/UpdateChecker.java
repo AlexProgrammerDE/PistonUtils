4f8225cf58e9ba2d770a01b54c5ef61c6285def0
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-01-15 19:31:03 +0100

    Use other system for web requests

--- src/main/java/net/pistonmaster/pistonutils/update/UpdateChecker.java
+++ src/main/java/net/pistonmaster/pistonutils/update/UpdateChecker.java
@@ -3,11 +3,15 @@ package net.pistonmaster.pistonutils.update;
 import net.pistonmaster.pistonutils.logging.ConsoleColor;
 import net.pistonmaster.pistonutils.logging.PistonLogger;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 @SuppressWarnings({"unused"})
 public class UpdateChecker {
@@ -18,12 +22,24 @@ public class UpdateChecker {
     }
 
     public void getVersion(String url, final Consumer<String> consumer) {
-        try (InputStream inputStream = new URL(url).openStream(); Scanner scanner = new Scanner(inputStream)) {
-            if (scanner.hasNext()) {
-                consumer.accept(scanner.next());
-            }
+        try {
+            consumer.accept(readURL(url, 5000));
         } catch (IOException exception) {
             log.info(ConsoleColor.RED_BOLD + "Cannot look for updates: " + exception.getMessage() + ConsoleColor.RESET);
+        }
+    }
+
+    private String readURL(String url, int timeout) throws IOException {
+        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", "PistonUtils");
+        con.setConnectTimeout(timeout);
+        con.setReadTimeout(timeout);
+        con.setDoOutput(true);
+
+        try (InputStream is = con.getInputStream()) {
+            return new BufferedReader(new InputStreamReader(is)).lines().collect(Collectors.joining());
         }
     }
 }
